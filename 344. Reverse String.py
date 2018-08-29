979344adc718af807b82cf13958147d75d30f9f0
jchen49gsu <jchen49gsu@gmail.com>
2018-08-28 21:33:03 -0700

    easy

---
+++ 344. Reverse String.py
@@ -0,0 +1,10 @@
+class Solution(object):
+	def reverse(self,S):
+		return S[::-1]
+
+s=Solution()
+S = "A man, a plan, a canal: Panama"
+print s.reverse(S)
+
+
+# 5 ways to do it https://www.geeksforgeeks.org/reverse-string-python-5-different-ways/
